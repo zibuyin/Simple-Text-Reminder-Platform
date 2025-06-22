d67fc37b20713e2ff2bcd451b630186c6fbbda2a
zibuyin <natdrone101@gmail.com>
2025-06-22 10:27:42 +0100

    Added popups

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,12 @@
-//---Date code---
-
-// Used to fetch and pharse current time
-function getTime() {
-    let time = new Date()
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
-    return {
-        "date": time,
-        "year": time.getFullYear(),
-        "month": time.getMonth() + 1,
-        "monthText": months[time.getMonth()],
-        "day": time.getDate(),
-        "dayText": days[time.getDay()],
-        "hour": time.getHours(),
-        "minutes": time.getMinutes(),
-        "seconds": time.getSeconds(),
-        "milisec": time.getMilliseconds()
-
+function profileClick() {
+    console.log("called")
+    var container = document.getElementById("super-popup-container");
+    console.log(container)
+    if (container.style.display === "block") {
+        console.log("displaying popup")
+        container.style.display = "none";
+    } else {
+        console.log("hiding popup")
+        container.style.display = "block";
     }
-
 }
-
-async function displayDate() {
-    dateNow = getTime()
-    display = dateNow.dayText + " " + dateNow.monthText + " " + dateNow.day + " " //Pharse it
-    document.getElementById('date').innerHTML = display //Dispay it (yee)
-}
-
-setInterval(displayDate, 1000)
-
-
-
-
-
-
