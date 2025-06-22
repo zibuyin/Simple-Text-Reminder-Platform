fecab954c3c0103afb0d0deb3d60381b17e9b0aa
zibuyin <natdrone101@gmail.com>
2025-06-22 15:47:19 +0100

    added popup contents

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,27 @@ function profileClick() {
         container.style.display = "block";
     }
 }
+function gdprClick() {
+    console.log("called")
+    var container = document.getElementById("gdpr-super-popup-container");
+    console.log(container)
+    if (container.style.display === "block") {
+        console.log("displaying popup")
+        container.style.display = "none";
+    } else {
+        console.log("hiding popup")
+        container.style.display = "block";
+    }
+}
+
+function dataDeletion() {
+    var confirm = prompt("Please type (case sensitive): I confirm that I want to delete all my notes")
+    if (confirm === "I confirm that I want to delete all my notes") {
+        alert("Confirmation succeeded. All notes are deleted")
+    }
+    else {
+        alert("Confirmation failed. aborting data deletion")
+
+
+    }
+}
