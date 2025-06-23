78eba0860829faf97d17515f94176310f4e9555a
zibuyin <natdrone101@gmail.com>
2025-06-23 14:36:33 +0100

    Added reminders logic

--- scripts/script.js
+++ scripts/script.js
@@ -34,3 +34,43 @@ function dataDeletion() {
 
     }
 }
+
+function submitEntry() {
+    var valueEntry = document.getElementsByClassName("entry-textarea")[0].value
+    var textArea = document.getElementsByClassName("entry-textarea")[0]
+    var noteID = parseInt(localStorage.getItem('noteCount')) + 1 //Increase the number by 1 to get current count
+    localStorage.setItem('noteCount', parseInt(noteID))
+    localStorage.setItem(noteID, valueEntry) //store the note
+    // console.log(valueEntry)
+    textArea.value = "" //Clear the text area after submition
+    displayNotes()
+
+
+}
+
+
+function displayNotes() {
+    var x = localStorage.getItem('0')
+    var testing = document.getElementsByClassName("entry-display")[0].innerHTML = x
+    for (var i = 0; i < localStorage.length; i++) {
+        console.log("Item: ", localStorage.getItem(localStorage.key(i)))
+    }
+    console.log("-----")
+}
+
+function setRemindersCounter() {
+    console.log(String(typeof (localStorage.length)))
+    document.getElementById("reminders-counter").innerHTML = "There are " + String(localStorage.length - 1) + " reminders"
+}
+
+
+
+// Initianlization
+localStorage.setItem('noteCount', -1)
+setRemindersCounter()
+
+displayNotes()
+//DEBUG DO NOT PUSH 
+// localStorage.clear()
+
+//DEBUG DO NOT PUSH 
